86902369e7d39c876a7ab2dfb8211b6754ec5921
jyothib <jyothibadavadagi@gmail.com>
2024-10-27 10:18:36 +0530

    all code and js assignment

---
+++ Day4/script.js
@@ -0,0 +1,16 @@
+var username = "gmit";
+var password = "gmit124";
+
+
+
+
+
+function InValidate() {
+    let inputUsername = document.getElementById("username").value;
+    let inputpassword = document.getElementById("password").value;
+}
+if (true) {
+    console.log("Success");
+} else {
+    console.log("Failure");
+}
